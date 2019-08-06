ebe7cb43dea47e7a072adb4da9b62cf280c30f40
Joilson Cisne <joilsoncisne@gmail.com>
2019-08-06 00:58:47 -0300

    Update eslint config

--- src/tron-battle/.eslintrc.js
+++ src/tron-battle/.eslintrc.js
@@ -1,4 +1,25 @@
 module.exports = {
   "extends": "eslint:recommended",
   "parser": "babel-eslint",
+  "env": {
+    "es6": true,
+    "node": true,
+    "browser": true
+  },
+  "rules": {
+    // enable additional rules
+    "indent": ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    "quotes": ["error", "single"],
+    "semi": ["error", "never"],
+
+    // override default options for rules from base configurations
+    "comma-dangle": ["error", "always-multiline"],
+    "no-cond-assign": ["error", "always"],
+    "no-constant-condition": "off",
+    // "max-len": ["error", {"code": 100, "tabWidth": 2, "ignoreUrls": true}]
+
+    // disable rules from base configurations
+    "no-console": "off",
+}
 }
