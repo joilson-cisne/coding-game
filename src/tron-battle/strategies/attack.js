e2fce4f4aedb87215e4de940c8df312131b023da
Joilson Cisne <joilsoncisne@gmail.com>
2019-08-06 00:59:32 -0300

    Fix lint issues

--- src/tron-battle/strategies/attack.js
+++ src/tron-battle/strategies/attack.js
@@ -1,34 +1,37 @@
 const nextStep = (myX, myY, oppX, oppY, parent) => {
   // console.error('me:', myX, myY) // DEBUG
   // console.error('opp:', oppX, oppY) // DEBUG
-  
+
   let { x, y } = parent[oppY][oppX]
   // console.error([x, y]) // DEBUG
   let nextX
   let nextY
-  
 
   while (x !== myX || y !== myY) {
-      nextX = x
-      nextY = y
-      
-      if (parent[nextY][nextX]) {
-          x = parent[nextY][nextX].x
-          y = parent[nextY][nextX].y
-      }
+    nextX = x
+    nextY = y
 
-      // console.error([x, y]) // DEBUG
+    if (parent[nextY][nextX]) {
+      x = parent[nextY][nextX].x
+      y = parent[nextY][nextX].y
+    }
+
+    // console.error([x, y]) // DEBUG
   }
-  
+
   return getNextDirection(myX, myY, nextX, nextY)
 }
 
 const getNextDirection = (myX, myY, nextX, nextY) => {
   return myX === nextX
-      ? myY < nextY ? 'DOWN' : 'UP'
-      : myX < nextX ? 'RIGHT' : 'LEFT'
+    ? myY < nextY
+      ? 'DOWN'
+      : 'UP'
+    : myX < nextX
+      ? 'RIGHT'
+      : 'LEFT'
 }
 
 export default {
-  nextStep
+  nextStep,
 }
