193d9263020d72128f6b8500183bb3e33e65b2b6
Joilson Cisne <joilsoncisne@gmail.com>
2019-08-10 16:13:40 -0300

    Use only the frontier strategy

--- tron-battle/src/bot.js
+++ tron-battle/src/bot.js
@@ -1,24 +1,7 @@
-import cuttingEdgeStrategy from './strategies/cuttingEdge'
 import frontierStrategy from './strategies/frontier'
 
-import { countVisitableNodes } from './utils'
-
-const isCuttingEdge = (myX, myY, globalGrid) => {
-  const counter = countVisitableNodes(myX, myY, globalGrid)
-
-  return Object.values(counter)
-    .filter(count => count > 0)
-    .some((val, i, arr) => val !== arr[0])
-}
-
-const nextStep = (me, opponents, grid) => {
-  const myX = me.x
-  const myY = me.y
-
-  return isCuttingEdge(myX, myY, grid)
-    ? cuttingEdgeStrategy.nextStep(myX, myY, grid)
-    : frontierStrategy.nextStep([me, ...opponents], grid)
-}
+const nextStep = (me, opponents, grid) =>
+  frontierStrategy.nextStep([me, ...opponents], grid)
 
 export default {
   nextStep,
