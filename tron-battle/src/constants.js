c3672df361368b3f77805ad21d2d8075bd670ede
Joilson Cisne <joilsoncisne@gmail.com>
2019-08-11 17:08:51 -0300

    Create direction vextors in constants.js

--- tron-battle/src/constants.js
+++ tron-battle/src/constants.js
@@ -5,3 +5,7 @@ export const LEFT = 'LEFT'
 
 export const WIDTH_LIMIT = 30
 export const HEIGHT_LIMIT = 20
+
+// UP, DOWN, LEFT, RIGHT direction vectors
+export const DX = [-1, 1, 0, 0]
+export const DY = [0, 0, -1, 1]
