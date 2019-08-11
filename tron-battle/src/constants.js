1cfdfe8461357e1eb3490af638312f44a5b79aa5
Joilson Cisne <joilsoncisne@gmail.com>
2019-08-11 20:04:07 -0300

    Fix direction vectors

--- tron-battle/src/constants.js
+++ tron-battle/src/constants.js
@@ -7,5 +7,5 @@ export const WIDTH_LIMIT = 30
 export const HEIGHT_LIMIT = 20
 
 // UP, DOWN, LEFT, RIGHT direction vectors
-export const DX = [-1, 1, 0, 0]
-export const DY = [0, 0, -1, 1]
+export const DX = [0, 0, -1, 1]
+export const DY = [-1, 1, 0, 0]
